98504e8987dd305a712894182f3fdadf5aaa6643
kovacsbalu <kovacsbalu@gmail.com>
2014-11-30 21:04:21 +0100

    Minor code cleaning. (pythonic way)

--- program.py
+++ program.py
@@ -1,20 +1,21 @@
 import homeauto as ha
 import time
 
-users = []
-with open('users', 'r') as u:
-    lines = u.readlines()
-    for l in lines:
-        users.append(ha.User.from_string(l))
+if __name__ == '__main__':
+    users = []
+    with open('users', 'r') as u:
+        lines = u.readlines()
+    for line in lines:
+        users.append(ha.User.from_string(line))
 
-presence = ha.Presence(users)
+    presence = ha.Presence(users)
 
-while True:
-    time.sleep(2)
-    aUsers = presence.get_presence()
-    with open('presence.log', 'a') as f:
-        for user in users:
-            ts = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-            entry = '%s: %s\n' % (ts, user)
-            f.write(entry)
-            print(entry)
+    while True:
+        time.sleep(2)
+        users_present = presence.get_presence()
+        with open('presence.log', 'a') as f:
+            for user in users_present:
+                ts = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+                entry = '%s: %s\n' % (ts, user)
+                f.write(entry)
+                print(entry)
