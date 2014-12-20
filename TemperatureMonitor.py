d7956ba2b7030e31e6b02badc71545f3b7e170ce
Andras Csehi <edcargo@gmail.com>
2014-12-20 11:24:41 +0000

    fix

--- TemperatureMonitor.py
+++ TemperatureMonitor.py
@@ -10,14 +10,14 @@ class TemperatureObservation(Entity):
 
 
 class TemperatureMonitor:
-    sensor = Adafruit_DHT.DHT22
-    pin = 6
 
     @staticmethod
-    def get_observation(self):
-        humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin)
+    def get_observation():
+        sensor = Adafruit_DHT.DHT11
+        pin = 4
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-        print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+        print temperature, humidity
         t = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 
         return TemperatureObservation(temperature,humidity,t)
